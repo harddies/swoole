b401a5c778afc10798d47fc7d213b06de6302c6a
Ronnie <harddies827@126.com>
2018-03-21 09:48:59 +0800

    swoole初始

---
+++ webServer/server/httpServer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ronnie
+ * Date: 2018/03/21
+ * Time: 09:39
+ */
+
+$http = new swoole_http_server("0.0.0.0", 9502);
+$http->on('request', function ($request, $response) {
+    var_dump($request->get, $request->post);
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end(file_get_contents(dirname(__FILE__) . '/../../webSocket/client/chat.html'));
+});
+$http->start();
