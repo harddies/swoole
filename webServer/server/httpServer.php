0c2173855b0c4064b9ae3e4067339fbe8b1c0bde
Ronnie <harddies827@126.com>
2018-03-21 13:37:00 +0800

    传参类型

--- webServer/server/httpServer.php
+++ webServer/server/httpServer.php
@@ -7,7 +7,7 @@
  */
 
 $http = new swoole_http_server("0.0.0.0", 9502);
-$http->on('request', function ($request, $response) {
+$http->on('request', function (swoole_http_request $request, swoole_http_response $response) {
     var_dump($request->get, $request->post);
     $response->header("Content-Type", "text/html; charset=utf-8");
     $response->end(file_get_contents(dirname(__FILE__) . '/../../webSocket/client/chat.html'));
--- webSocket/server/chatServer.php
+++ webSocket/server/chatServer.php
@@ -2,11 +2,11 @@
 
 $server = new swoole_websocket_server("0.0.0.0", 9501);
 
-$server->on('open', function (swoole_websocket_server $server, $request) {
+$server->on('open', function (swoole_websocket_server $server, swoole_http_request $request) {
     echo "server: handshake success with fd{$request->fd}\n";
 });
 
-$server->on('message', function (swoole_websocket_server $server, $frame) {
+$server->on('message', function (swoole_websocket_server $server, swoole_websocket_frame $frame) {
     foreach($server->connections as $key => $fd) {
         $user_message = $frame->data;
         $server->push($fd, $user_message);
@@ -14,7 +14,7 @@ $server->on('message', function (swoole_websocket_server $server, $frame) {
 
 });
 
-$server->on('close', function ($ser, $fd) {
+$server->on('close', function (swoole_websocket_server $server, $fd) {
     echo "client {$fd} closed\n";
 });
 
